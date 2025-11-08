354739d4373f21d52754169f843182a316ee4ba1
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-11-08 23:06:05 +0900

    add:掲示板作成画面の表示まで実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,19 @@ class BoardsController < ApplicationController
     def index
         @boards = Board.includes(:user)
     end
+
+    def new
+        @board = Board.new
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+            flash[:success] = "Board created successfully."
+            redirect_to boards_path
+        else
+            @boards = Board.includes(:user)
+            render 'index'
+        end
+    end
 end
