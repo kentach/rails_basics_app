7f23bdb08dd2533abdb069814121a6a9e5e21d8c
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-11-09 12:18:29 +0900

    バリデーションメッセージの表示

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,13 +1,16 @@
 class UserSessionsController < ApplicationController
     skip_before_action :require_login, only: %i[new create]
 
-    def new; end
+    def new
+        @user = User.new
+    end
 
     def create
         @user = login(params[:email], params[:password])
         if @user
             redirect_to root_path, success: t('user_sessions.create.success')
         else
+            @user ||= User.new
             flash.now[:danger] = t('user_sessions.create.failure')
             render :new, status: :unprocessable_entity
         end
